1e3a4c4c83c96b220c71c8714a5cce2e2d7b60ab
CHRISTIANDAGATTI <dagattichristian@gmail.com>
2025-05-17 14:52:24 -0300

    Se agrega funcion matar proceso

--- procesos.sh
+++ procesos.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
 # Función para mostrar procesos en ejecución
 ver_procesos() {
     ps aux | less
 }
+# Función para ver info de un proceso
 info_proceso() {
     read -p "Ingrese el PID del proceso: " pid
     if ! [[ $pid =~ ^[0-9]+$ ]]; then
@@ -16,8 +16,23 @@ info_proceso() {
     ps -p $pid -o pid,ppid,cmd,%mem,%cpu,etime
     echo "----------------------------------------"
 }
+# Función para frenar un proceso (kill)
+matar_proceso() {
+    read -p "Ingrese el PID del proceso a terminar: " pid
+    if ! [[ "$pid" =~ ^[0-9]+$ ]]; then
+        echo "⚠️  **PID no válido**. Debe ser un número."
+        exit 1
+    fi
+    if kill -9 "$pid" 2>/dev/null; then
+        echo "**Proceso $pid terminado correctamente.**"
+    else
+        echo "⚠️  **No se pudo terminar el proceso** (verifique el PID o los permisos)."
+    fi
+}
+
 case $1 in
     ver_procesos) ver_procesos ;;
     info_proceso) info_proceso;;
+    matar_proceso) matar_proceso;;
     *) echo "Opción inválida desde procesos.sh" ;;
 esac
