ff1c14147c65e721cd62f02a36dd745479a36a64
adriel1364 <adridelosanto@gmail.com>
2025-05-17 11:41:35 -0300

    agregado info procesos

--- procesos.sh
+++ procesos.sh
@@ -4,8 +4,20 @@
 ver_procesos() {
     ps aux | less
 }
-
+info_proceso() {
+    read -p "Ingrese el PID del proceso: " pid
+    if ! [[ $pid =~ ^[0-9]+$ ]]; then
+        echo "⚠️  PID no válido. Debe ser un número."
+        exit 1
+    fi
+    clear
+    echo "Información del proceso con PID $pid:"
+    echo "----------------------------------------"
+    ps -p $pid -o pid,ppid,cmd,%mem,%cpu,etime
+    echo "----------------------------------------"
+}
 case $1 in
     ver_procesos) ver_procesos ;;
+    info_proceso) info_proceso;;
     *) echo "Opción inválida desde procesos.sh" ;;
 esac
